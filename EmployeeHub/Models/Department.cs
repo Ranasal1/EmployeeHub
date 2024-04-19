a73186cae5753d9720f52ba0480e7a58e59bbc2d
Rana <Rana@DESKTOP-62LSES4>
2024-04-19 13:56:06 -0700

    Add project files.

---
+++ EmployeeHub/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeHub.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ EmployeeHub/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeHub.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+        public string Address { get; set; }
+    }
+}
