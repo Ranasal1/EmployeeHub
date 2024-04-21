8e944a8ac0669f6adbc963ed1049e718d06b1ccc
Ranasal1 <rana.saleh1000@gmail.com>
2024-04-21 15:54:53 +0300

    #44 Seed Admin Data

--- EmployeeHub/Pages/Index.cshtml.cs
+++ EmployeeHub/Pages/Index.cshtml.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace EmployeeHub.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
