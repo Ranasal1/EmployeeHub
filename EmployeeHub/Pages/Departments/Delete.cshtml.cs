d4d335dcfff174a83b5446af4c6fb3ace2af17e9
441020958 <441020958@sm.imamu.edu.sa>
2024-04-22 00:44:13 -0700

    #62 Delete Department

---
+++ EmployeeHub/Pages/Departments/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EmployeeHub.Data;
+using EmployeeHub.Models;
+
+namespace EmployeeHub.Pages.Departments
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly EmployeeHub.Data.ApplicationDbContext _context;
+
+        public DeleteModel(EmployeeHub.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Department Department { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Department.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Department = department;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Department.FindAsync(id);
+            if (department != null)
+            {
+                Department = department;
+                _context.Department.Remove(Department);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
