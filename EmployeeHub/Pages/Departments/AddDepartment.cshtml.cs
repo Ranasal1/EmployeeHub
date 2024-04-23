ae29d8434458ec20b8915b133c11bdade54e920b
Ranasal1 <rana.saleh1000@gmail.com>
2024-04-23 10:45:22 +0300

    #51 Add Department

---
+++ EmployeeHub/Pages/Departments/AddDepartment.cshtml.cs
@@ -0,0 +1,35 @@
+using EmployeeHub.Data;
+using EmployeeHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeHub.Pages.Departments
+{
+    public class AddDepartmentModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        [BindProperty]
+        public Department Department { get; set; }
+
+        public AddDepartmentModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Department.AddAsync(Department);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./ViewDepartment");
+            }
+            return Page();
+        }
+
+    }
+}
