9c09f3266d4be25bd2f7cdae9bb12a41552f0647
Ranasal1 <rana.saleh1000@gmail.com>
2024-04-23 09:22:56 +0300

    #59 View Department

---
+++ EmployeeHub/Pages/Departments/ViewDepartment.cshtml.cs
@@ -0,0 +1,23 @@
+using EmployeeHub.Data;
+using EmployeeHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeHub.Pages.Departments
+{
+    public class ViewDepartmentModel : PageModel
+    {
+        public List<Department> departments = new List<Department>();
+
+        private readonly ApplicationDbContext _context;
+
+        public ViewDepartmentModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet()
+        {
+            departments = _context.Department.ToList();
+        }
+    }
+}
