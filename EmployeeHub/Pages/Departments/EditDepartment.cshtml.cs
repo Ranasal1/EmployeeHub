dba58a0c5c9f56edfad9516d17eb868d123aa662
Ranasal1 <rana.saleh1000@gmail.com>
2024-04-27 21:20:47 +0300

    #57

--- EmployeeHub/Pages/Departments/EditDepartment.cshtml.cs
+++ EmployeeHub/Pages/Departments/EditDepartment.cshtml.cs
@@ -23,16 +23,45 @@ namespace EmployeeHub.Pages.Departments
             }
             return Page();
         }
-        public IActionResult OnPost(Department department)
+         public IActionResult OnPost(Department department)
         {
             Department = _context.Department.FirstOrDefault(x => x.Id == department.Id);
             if (ModelState.IsValid && Department != null)
             {
+                // Capture original department details
+                var originalDepartment = new Department
+                {
+                    Name = Department.Name,
+                    Description = Department.Description,
+                    ContactInformation = Department.ContactInformation,
+                    Address = Department.Address
+                };
+
                 Department.Name = department.Name;
                 Department.Description = department.Description;
                 Department.ContactInformation = department.ContactInformation;
                 Department.Address = department.Address;
+
                 _context.SaveChanges();
+
+                // Check if department details have changed
+                if (!Department.Equals(originalDepartment))
+                {
+                    var existingAuditRecord = _context.DepartmentAudit.FirstOrDefault(a => a.DepartmentId == Department.Id);
+
+                    if (existingAuditRecord != null)
+                    {
+                        existingAuditRecord.ModifiedDate = DateTime.Now;
+                        existingAuditRecord.ModifierName = User.Identity.Name;
+
+                        _context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No changes in department details.");
+                }
+
                 return RedirectToPage("./ViewDepartment");
             }
             return Page();
